04c7b3df496df0c69b804d45624520b132e6c191
goldencat-tom <bluebird6922@gmail.com>
2021-12-15 18:12:21 +0900

    wallet context

---
+++ src/contexts/Solana/Wallet.tsx
@@ -0,0 +1,184 @@
+import {
+  SignerWalletAdapter,
+  WalletAdapterNetwork,
+  WalletError,
+} from '@solana/wallet-adapter-base';
+import {
+  useWallet as useWalletBase,
+  WalletContextState,
+  WalletProvider as BaseWalletProvider
+} from '@solana/wallet-adapter-react';
+import {
+  getLedgerWallet,
+  getPhantomWallet,
+  getSlopeWallet,
+  getSolflareWallet,
+  getSolletWallet,
+  getSolletExtensionWallet,
+  getTorusWallet,
+} from '@solana/wallet-adapter-wallets';
+import { Button, Modal } from "antd";
+import { createContext, ReactNode, useCallback, useContext, useEffect, useMemo, useState } from 'react';
+import { notify } from "../Utils";
+import { useConnectionConfig } from "./Connection";
+
+export function useWallet (): WalletContextState {
+  return useWalletBase();
+}
+
+export type WalletSigner = Pick<SignerWalletAdapter, 'publicKey' | 'signTransaction' | 'signAllTransactions'>;
+
+export interface WalletModalContextState {
+  visible: boolean;
+  setVisible: (open: boolean) => void;
+}
+
+export const WalletModalContext = createContext<WalletModalContextState>({} as WalletModalContextState);
+
+export function useWalletModal(): WalletModalContextState {
+  return useContext(WalletModalContext);
+}
+
+export const WalletModal = () => {
+  const { wallets, wallet: selected, select } = useWallet();
+  const { visible, setVisible } = useWalletModal();
+  const close = useCallback(() => setVisible(false), [setVisible]);
+
+  return (
+    <Modal
+      title="Select Wallet"
+      okText="Connect"
+      visible={visible}
+      footer={null}
+      onCancel={close}
+      width={400}
+    >
+      {wallets.map((wallet) => {
+        return (
+          <Button
+            key={wallet.name}
+            size="large"
+            type={wallet === selected ? 'primary' : 'ghost'}
+            onClick={() => { select(wallet.name); close(); }}
+            icon={
+              <img
+                alt={`${wallet.name}`}
+                width={20}
+                height={20}
+                src={wallet.icon}
+                style={{marginRight: 8}}
+              />
+            }
+            style={{
+              display: 'block',
+              width: '100%',
+              textAlign: 'left',
+              marginBottom: 8,
+            }}
+          >
+            {wallet.name}
+          </Button>
+        );
+      })}
+    </Modal>
+  );
+};
+
+export const WalletModalProvider = ({ children }: { children: ReactNode }) => {
+  const { publicKey } = useWallet();
+  const [connected, setConnected] = useState(!!publicKey);
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    if (publicKey) {
+      const base58 = publicKey.toBase58();
+      const keyToDisplay =
+        base58.length > 20
+          ? `${base58.substring(
+            0,
+            7,
+          )}.....${base58.substring(
+            base58.length - 7,
+            base58.length,
+          )}`
+          : base58;
+
+      notify({
+        message: 'Wallet update',
+        description: 'Connected to wallet ' + keyToDisplay,
+      });
+    }
+  }, [publicKey]);
+
+  useEffect(() => {
+    if (!publicKey && connected) {
+      notify({
+        message: 'Wallet update',
+        description: 'Disconnected from wallet',
+      });
+    }
+    setConnected(!!publicKey);
+  }, [publicKey, connected, setConnected]);
+
+  return (
+    <WalletModalContext.Provider
+      value={{
+        visible,
+        setVisible,
+      }}
+    >
+      {children}
+      <WalletModal/>
+    </WalletModalContext.Provider>
+  );
+};
+
+export const WalletProvider = ({ children }: {children: ReactNode }) => {
+  const { env } = useConnectionConfig();
+
+  const network = useMemo(() => {
+    switch (env) {
+      case "mainnet-beta":
+        return WalletAdapterNetwork.Mainnet;
+      case "testnet":
+        return WalletAdapterNetwork.Testnet;
+      case "devnet":
+      case "localnet":
+      default:
+        return WalletAdapterNetwork.Devnet;
+    }
+  }, [env]);
+
+  const wallets = useMemo(
+    () => [
+      getPhantomWallet(),
+      getSlopeWallet(),
+      getSolflareWallet(),
+      getTorusWallet(),
+      getLedgerWallet(),
+      getSolletWallet({ network }),
+      getSolletExtensionWallet({ network }),
+    ],
+    [network]
+  );
+
+  const onError = useCallback((error: WalletError) => {
+    console.error(error);
+    notify({
+      message: 'Wallet error',
+      description: error.message,
+    });
+  }, []);
+
+  return (
+    <BaseWalletProvider
+      wallets={wallets}
+      onError={onError}
+      autoConnect
+    >
+      <WalletModalProvider>
+        {children}
+      </WalletModalProvider>
+    </BaseWalletProvider>
+  );
+}
---
+++ src/contexts/Utils/Notifications.tsx
@@ -0,0 +1,23 @@
+import { notification } from 'antd';
+
+export function notify({
+  message = '',
+  description = undefined as any,
+  txid = '',
+  type = 'info',
+  placement = 'bottomLeft',
+}) {
+  if (txid) {
+    description = <></>;
+  }
+  (notification as any)[type]({
+    message: <span style={{ color: 'black' }}>{message}</span>,
+    description: (
+      <span style={{ color: 'black', opacity: 0.5 }}>{description}</span>
+    ),
+    placement,
+    style: {
+      backgroundColor: 'white',
+    },
+  });
+}
